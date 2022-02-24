e221525258adb811bcacd8e9e163ee5257900e87
Chris Vazquez <cvg_v10@hotmail.com>
2022-02-23 23:48:50 -0800

    we created guidelines to creating a password

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,31 @@
+let hasLower
+let hasUpper
+let hasNumbers
+let hasSpecChars
+let passlength
+
+
+const passPromt = () => {
+  passlength = parseInt(prompt('how long do you want your password'))
+  hasLower = confirm('do you want to lowercase character')
+  hasUpper =confirm('do you want to uppercase character')
+  hasNumbers=confirm('do you want to Number character')
+  hasSpecChars=confirm('do you want special Character')
+  console.log(passlength,hasLower,hasUpper,hasNumbers,hasSpecChars,);
+  if(isNaN(passlength)=true || passlength < 8||     passlength > 128){
+    alert('your password needs to be a number between 8 and 128')
+    passPromt()
+  }
+}
+
+
+
+
+
+
+
+
 document.getElementById('generate').addEventListener('click', () => {
+  passPromt()
 
 })
