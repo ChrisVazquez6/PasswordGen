44ea2e00fe2c3148f5958c23fec9a387637d0f29
Chris Vazquez <cvg_v10@hotmail.com>
2022-02-24 01:29:22 -0800

    create password on console

--- Develop/script.js
+++ Develop/script.js
@@ -24,6 +24,7 @@ const passGenerate = ()=> {
   let numString = '0123456789'
   let specString = '!@#$%^&*()'
   let chosenString = ''
+  let password = ''
 
   if (hasLower === true) {
     chosenString += lowerString
@@ -39,13 +40,36 @@ const passGenerate = ()=> {
   }
   console.log(chosenString)
 
-
-
-
-
-
-
+  for (let i = 0; i < passlength; i++) {
+    let randomIndex = Math.floor(Math.random() * chosenString.length)
+    let randomChar = chosenString[randomIndex]
+    // console.log(randomChar);
+    password = password + randomChar
+  }
+  console.log(password);
+  return password
 }
+// let tentativePassword
+// const passVerify = ()=>{
+//  tentativePassword = passGenerate()
+//  if(hasLower===true){
+//    let verifiedLower = false
+//    for (let i = 0; i< passlength; i++) {
+//      let possibleLower = tentativePassword[i]
+//      console.log(possibleLower)
+//      if (possibleLower==='a'||possibleLower==='b'||possibleLower==='c'||possibleLower==='d'||possibleLower==='e'||possibleLower==='f'||possibleLower==='g'||possibleLower==='h'||possibleLower==='i'||possibleLower==='j'||possibleLower==='k'||possibleLower==='l'||possibleLower==='m'||possibleLower==='n'||possibleLower==='o'||possibleLower==='p'||possibleLower==='q'||possibleLower==='r'||possibleLower==='s'||possibleLower==='t'||
+//      possibleLower==='u'||possibleLower==='v'||possibleLower==='w'||possibleLower==='x'||possibleLower==='y'||possibleLower==='z') {
+//        verifiedLower=true
+//      }
+//     }
+//      if (verifiedLower === false) {
+//        console.log('no lowercase characters');
+//        passVerify()
+//      }
+
+       
+     
+    
 
 
 
@@ -56,6 +80,10 @@ const passGenerate = ()=> {
 
 document.getElementById('generate').addEventListener('click', () => {
   passPromt()
-  passGenerate()
+  // passGenerate()
+  let newpassword = passGenerate()
+  console.log(newpassword);
+  document.getElementById('password').innerHTML=newpassword
+
 
 })
